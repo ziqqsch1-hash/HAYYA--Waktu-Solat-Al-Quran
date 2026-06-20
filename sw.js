e4d822635487a80f5fe4fd8d0025cdc4c187175a
ziqqsch1-hash <ziqqsch1@gmail.com>
2026-06-20 12:39:59 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'hayya-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Peringkat Pemasangan (Install)
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Peringkat Pengaktifan (Activate)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Peringkat Pengambilan Data (Fetch)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
